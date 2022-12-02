d072721a1fc1b0c3347365080f3eb3c5ce61f041
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-12-02 16:32:39 +0300

    Day 2: star 1

---
+++ 22/day2/x.ts
@@ -0,0 +1,58 @@
+export const x = '';
+
+import { readFileSync } from 'fs';
+
+interface IOutcome {
+    opp: string;
+    me: string;
+}
+
+const input: string = readFileSync('input.txt', 'utf8');
+
+let data = input.split('\r\n');
+
+let outcomes: IOutcome[] = data.map(pair => { return {opp: pair[0], me: pair[2]} });
+
+// B defeats X
+// C defeats Y
+// A defeats Z
+
+function evaluate(outcome: IOutcome): number {
+    let score: number = 0;
+    const [ opp, me ] = [ outcome.opp, outcome.me ];
+    switch (me){
+        case 'X':
+            score += 1
+            break;
+        case 'Y':
+            score += 2
+            break;
+        case 'Z':
+            score += 3
+            break;
+    }
+
+    if(
+        (me === 'X' && opp === 'C') ||
+        (me === 'Y' && opp === 'A') ||
+        (me === 'Z' && opp === 'B')){
+        // I win
+        score += 6;
+    } 
+    
+    else if (
+        (me === 'X' && opp === 'A') ||
+        (me === 'Y' && opp === 'B') ||
+        (me === 'Z' && opp === 'C')){
+        // Draw
+        score += 3;
+    }
+
+    return score;        
+}
+
+let totalScores: number = outcomes
+    .map(outcome => evaluate(outcome))
+    .reduce((a, b) => a + b);
+
+console.log(totalScores);
