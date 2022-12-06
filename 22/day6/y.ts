f4ed43dc72cb2e2f824731daab1262662b37a79f
Austin Musiku <47279609+MusikuAustin@users.noreply.github.com>
2022-12-07 02:25:12 +0300

    Day 6: star 2

---
+++ 22/day6/y.ts
@@ -0,0 +1,31 @@
+export const x = '';
+
+import { readFileSync } from 'fs';
+
+const input: string = readFileSync('input.txt', 'utf8');
+
+const data: string[] = input.split('');
+
+const buffer: string[] = []
+const MARKER_SIZE: number = 14
+
+let isFound = false
+let start: number = 0
+
+function checkForPacketMarker(char: string): void{
+    buffer.push(char)
+    
+    if(buffer.length > MARKER_SIZE) buffer.shift()
+    
+    const bufferSet: Set<string> = new Set(buffer)
+
+    if(bufferSet.size === MARKER_SIZE) isFound = true
+}
+
+for(let char of data) {
+    if(isFound) break
+    start++
+    checkForPacketMarker(char)
+}
+
+console.log(start)
